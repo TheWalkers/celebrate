49375386005bd6cdb31db91c373e2dc171814741
Gareth Mackrill <gmackrill@ILP05MBP028.internal.ngpvan.net>
2022-04-16 13:52:19 -0500

    option to not watch

--- robot/boot.py
+++ robot/boot.py
@@ -10,5 +10,22 @@ import webrepl
 webrepl.start()
 gc.collect()
 
+import connect
+connect.do_connect()
+
 import ntptime
 ntptime.settime()
+
+should_watch = True
+import urequests
+try:
+    debug_req = urequests.get('http://ak-celebrate.s3-us-west-2.amazonaws.com/no_watch.html')
+    if debug_req.status_code == 200:
+        should_watch = False
+except:
+    pass
+
+if should_watch:
+    import celebrate
+    celebrate.watch()
+
